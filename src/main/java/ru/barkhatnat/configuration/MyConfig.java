06c9bda78d483ca0dfb33de395e7ce95b9c21d6a
Natalia Barkhatova <natabarkhatova@yandex.ru>
2024-03-06 20:35:39 +0300

    feat: add java configuration

---
+++ src/main/java/ru/barkhatnat/configuration/MyConfig.java
@@ -0,0 +1,53 @@
+package ru.barkhatnat.configuration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan("ru.barkhatnat")
+@EnableWebMvc
+@EnableTransactionManagement
+public class MyConfig {
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("org.postgresql.Driver");
+            dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/finsmart");
+            dataSource.setUser("postgres");
+            dataSource.setPassword("ilovecookie");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("ru.barkhatnat.entity");
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        sessionFactory.setHibernateProperties(hibernateProperties);
+        return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+}
