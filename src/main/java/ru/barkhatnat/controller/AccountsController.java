8bfaacc2fe062cda282f69af784c1db51c3c84e0
Natalia Barkhatova <natabarkhatova@yandex.ru>
2024-04-03 23:13:15 +0300

    feat: links to buttons

--- src/main/java/ru/barkhatnat/controller/AccountsController.java
+++ src/main/java/ru/barkhatnat/controller/AccountsController.java
@@ -1,32 +1,31 @@
 package ru.barkhatnat.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.barkhatnat.controller.payload.NewAccountPayload;
 import ru.barkhatnat.service.AccountService;
 
-import java.util.NoSuchElementException;
-
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/accounts")
 public class AccountsController {
     private final AccountService accountService;
+
     @GetMapping("/list")
-    public String getAccountsList(Model model){
-        model.addAttribute("accounts", accountService.getAllAccounts());
+    public String getAccountsList(Model model) {
+        model.addAttribute("accounts", accountService.findAllAccounts());
         return "account-list";
     }
+
     @GetMapping("/create")
-    public String createAccount(){
+    public String createAccount() {
         return "account-creation";
     }
+
     @PostMapping("/save")
-    public String saveAccount(NewAccountPayload payload){
+    public String saveAccount(NewAccountPayload payload) {
         accountService.createAccount(payload.title(), payload.balance());
         return "redirect:/accounts/list";
     }
